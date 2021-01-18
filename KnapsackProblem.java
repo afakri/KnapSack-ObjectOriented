a10c0412724d051b3b6e4552eb3e7bcad9b63e76
ayman fakri <61848289+afakri@users.noreply.github.com>
2021-01-17 22:32:15 -0500

    bruteforce return the correct best value.

--- KnapsackProblem.java
+++ KnapsackProblem.java
@@ -2,19 +2,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 public class KnapsackProblem{
-    private int numberItems;
-    private String approach ;
-    private Knapsack sac;
+    private static int numberItems;
+    private static String approach ;
+    private static Knapsack bestSac;
+    private static Item[] items;
+    private static String bestSol ="";
+    private static int  cap;
+    private static int val=0;
 
 
-    public void initialize(BufferedReader in){
+    public static void initialize(BufferedReader in){
         try {
             numberItems = Integer.parseInt(in.readLine());
+            items = new Item[numberItems];
             for(int i=0;i<numberItems;i++){
                 String[] info = in.readLine().split(" ");
                 Item item= new Item(info[0],Integer.parseInt(info[1]),Integer.parseInt(info[2]));
+                items[i]=item;
             }
-            sac = new Knapsack(Integer.parseInt(in.readLine()));
+            bestSac = new Knapsack(Integer.parseInt(in.readLine()));
+            cap=bestSac.getCapacity();
             in.close();
         } catch (Exception e) {
             System.out.println(e);
@@ -22,8 +29,25 @@ public class KnapsackProblem{
         
 
     }
-    public void bruteForce(){
-        //*******TODO********
+    public static int bruteForce(int index,String sol, int c){
+        int vin,vex;
+        if(index==numberItems || c<=0){
+            return 0;
+        }
+        if(items[index].getWeight()<=c){
+            vin=items[index].getValue()+bruteForce(index+1, sol,c-items[index].getWeight());
+            vex=bruteForce(index+1, sol,c);
+            return Math.max(vin,vex);
+        }
+        else{
+            
+            vex = bruteForce(index+1, sol,c);
+            return vex;
+        }
+        
+
+        
+
     }
     public void dynamic(){
         //*******TODO*********
@@ -31,12 +55,14 @@ public class KnapsackProblem{
     
     public static void main(String[] args) {
         BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        initialize(sc);
+        System.out.println(bruteForce(0,"",cap));
 
     }
 
 
 
-    class Knapsack{
+    static class Knapsack{
 
         private Stack<Item> items;
         private int capacity;
@@ -62,9 +88,18 @@ public class KnapsackProblem{
             value-=item.getValue();
             capacity+=item.getWeight();
         }
+
+        public int getValue(){
+            return value;
+        }
+
+        public int getCapacity(){
+            return capacity;
+        }
+
     }
 
-    class Item{
+    static class Item{
 
         private int value;
         private int weight;
